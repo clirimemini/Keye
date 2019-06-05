368c70151c481f23fe550bc0dd1061dd19440134
Çlirim Emini <40791655+clirimemini@users.noreply.github.com>
2019-06-05 21:09:23 +0200

    Update slackconfig.py

--- slackconfig.py
+++ slackconfig.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 
 #Slack webhook for notifications
-posting_webhook = "https://hooks.slack.com/services/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"
+posting_webhook = "https://hooks.slack.com/services/<secret>"
